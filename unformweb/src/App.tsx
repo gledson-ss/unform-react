aded8db6ed861fae36e6e318b7aafd730f77e747
GledsonS831 <glegogle84@gmail.com>
2020-07-14 11:07:59 -0300

    :hammer: chore(unformweb): create unform with react

--- unformweb/src/App.tsx
+++ unformweb/src/App.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Form } from "@unform/web";
+import { Scope, FormHandles } from "@unform/core";
 import "./App.css";
 
+import Input from "./components/Form/input";
+
+const initialData = {
+  password: "password",
+};
+
 function App() {
+  const formRef = useRef(null);
+
+  function handleSubmit(data: React.InputHTMLAttributes<HTMLInputElement>) {
+    console.log(data.name);
+  }
+
   return (
     <div className="App">
       <h1>hello unform</h1>
+
+      <Form initialData={initialData} onSubmit={handleSubmit}>
+        <Input name="name" />
+        <Input name="email" />
+        <Scope path="address">
+          <Input name="password" type="password" />
+          <Input name="street" />
+          <Input name="number" />
+        </Scope>
+
+        <button type="submit">Send</button>
+      </Form>
     </div>
   );
 }
---
+++ unformweb/src/components/Form/input.tsx
@@ -0,0 +1,31 @@
+import { readFileSync } from "fs";
+
+import React, { useEffect, useRef } from "react";
+import { useField } from "@unform/core";
+
+interface InputProps {
+  name: string;
+  type?: string;
+}
+
+const Input: React.FC<InputProps> = ({ name, ...rest }) => {
+  const inputRef = useRef(null);
+  const { fieldName, registerField, defaultValue, error } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value",
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <div>
+      <input ref={inputRef} {...rest} defaultValue={defaultValue} />;
+      {error && <span style={{ color: "#044" }}>{error}</span>}
+    </div>
+  );
+};
+
+export default Input;
